adb858cdbee109c0af24b7f6fd40b85f93574842
hannah-taylor1 <56603407+hannah-taylor1@users.noreply.github.com>
2019-10-15 12:30:23 -0600

    Add files via upload

---
+++ script/tickets.js
@@ -0,0 +1,6 @@
+function analyzeAge() {
+    var a = document.getElementById("myAge").value;
+    if (a>=16){
+    document.getElementById("cost").innerHTML = "Your price is: <br><br> $35.00";
+  }
+  }
